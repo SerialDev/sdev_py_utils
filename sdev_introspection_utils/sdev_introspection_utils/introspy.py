1323c8d79aab7042d7540721c0f6b0c3bc2a8a15
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-06-19 15:58:48 +0300

    [+] try-catch using the ast library to reduce code noise

--- sdev_introspection_utils/sdev_introspection_utils/introspy.py
+++ sdev_introspection_utils/sdev_introspection_utils/introspy.py
@@ -1,3 +1,4 @@
+
 import types
 import inspect
 import doctest
---
+++ sdev_pycore_utils/sdev_pycore_utils/ast_utils.py
@@ -0,0 +1,22 @@
+import ast
+
+def try_catch(funcall ):
+    """
+    try-catch wrapper using the AST library to reduce code noise
+
+    Parameters
+    ----------
+
+    funcall : str
+       String representation of the code to be evaluated
+
+    Returns
+    -------
+
+    None
+       nil
+    """
+    try:
+        ast.literal_eval(funcall)
+    except Exception as e:
+        print("{} : failed to execute".format(funcall))
