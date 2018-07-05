efedcee951377521d24b76080c2eb50752e34d0d
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-07-05 13:14:42 +0300

    [+] AWS utils binary file streaming support for loading and dumping

--- sdev_lib_utils/sdev_lib_utils/aws_utils.py
+++ sdev_lib_utils/sdev_lib_utils/aws_utils.py
@@ -3,6 +3,7 @@ import json
 import boto3
 import dill as pickle
 import zlib
+import io
 
 class aws_utils(object):
     def __init__(self, key, secret):
@@ -29,9 +30,11 @@ class aws_utils(object):
         self.current_bucket=  self.get_s3_res().Bucket(bucket_name)
         return self.current_bucket
 
+
     def get_bucket_key(self, bucket_name, key_name):
         self.current_bucket_key=  self.get_s3_client().get_object(Bucket=bucket_name, Key=key_name)
         return self.current_bucket_key
+
 
     def get_bucket_key_json(self):
         if hasattr(self, 'current_bucket_key'):
@@ -59,6 +62,44 @@ class aws_utils(object):
     def from_s3(self, bucket_name, key_name):
         return pickle.loads(zlib.decompress(self.get_s3_client().get_object(Bucket=bucket_name, Key=key_name)['Body'].read()))
 
+
+
+    def from_bin_file_streaming(name, bucket_name, key_name, full_path=False):
+        if full_path:
+            path = name
+        else:
+            path = os.path.join(os.getcwd(), name)
+
+        with open(path), 'ab') as f:
+            obj = self.get_s3_client().get_object(Bucket=bucket_name, Key=key_name)['Body'].iter_lines()
+            for i in obj:
+                f.write(i)
+
+
+    def from_bin_streaming(bucket_name, key_name):
+        out_buffer = io.BytesIO()
+        obj = self.get_s3_client().get_object(Bucket=bucket_name, Key=key_name)['Body'].iter_lines()
+        for i in obj:
+            out_buffer.write(i)
+        out_buffer.seek(0)
+        return out_buffer
+
+    def to_bin_streaming(data, bucket_name, key_name):
+        out_buffer = io.BytesIO()
+        out_buffer.write(data)
+        result = self.get_s3_client().upload_fileobj(out_buffer, bucket_name, key_name)
+        return result
+
+
+    def read_bin(name, full_path=False):
+        if full_path:
+            path = name
+        else:
+            path = os.path.join(os.getcwd(), name)
+
+        with open(path), 'rb') as f:
+            obj = f.read()
+        return obj
 
     def iter_to_s3(self, bucket_name, iterable, key_name, increments=50):
 
