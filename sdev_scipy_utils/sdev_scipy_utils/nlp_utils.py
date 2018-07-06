e75023c91fd65383286f199e2a9496b024d23793
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-07-06 13:09:47 +0300

    [+] λ Apply Burrows Wheeler Transform

--- sdev_scipy_utils/sdev_scipy_utils/nlp_utils.py
+++ sdev_scipy_utils/sdev_scipy_utils/nlp_utils.py
@@ -261,3 +261,32 @@ def pad_sentences(sentences, padding_word="<PAD/>"):
         padded_sentences.append(new_sentence)
     return padded_sentences
 
+
+# From https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
+def bwt(s):
+    """Apply Burrows-Wheeler transform to input string. Not indicated by a unique byte but use index list"""
+    # Table of rotations of string
+    table = [s[i:] + s[:i] for i in range(len(s))]
+    # Sorted table
+    table_sorted = table[:]
+    table_sorted.sort()
+    # Get index list of ((every string in sorted table)'s next string in unsorted table)'s index in sorted table
+    indexlist = []
+    for t in table_sorted:
+        index1 = table.index(t)
+        index1 = index1+1 if index1 < len(s)-1 else 0
+        index2 = table_sorted.index(table[index1])
+        indexlist.append(index2)
+    # Join last characters of each row into string
+    r = ''.join([row[-1] for row in table_sorted])
+    return r, indexlist
+
+
+def ibwt(r,indexlist):
+    """Inverse Burrows-Wheeler transform. Not indicated by a unique byte but use index list"""
+    s = ''
+    x = indexlist[0]
+    for _ in r:
+        s = s + r[x]
+        x = indexlist[x]
+    return s
