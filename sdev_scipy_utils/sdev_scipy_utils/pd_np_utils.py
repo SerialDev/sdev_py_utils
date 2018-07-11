13150c3c202965ad0100148af75df5b6158913b4
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-07-11 13:02:10 +0300

    [+] f() np_parallel_apply_along

--- sdev_scipy_utils/sdev_scipy_utils/pd_np_utils.py
+++ sdev_scipy_utils/sdev_scipy_utils/pd_np_utils.py
@@ -1141,3 +1141,24 @@ def pd_parallel_apply(data, col_name, func, num_partitions=10):
     pool.close()
     pool.join()
     return data
+
+
+
+def np_parallel_apply_along(data, function, axis=1, parts=4, threads=4):
+    #WARNING TODO: use ast to use only the given parts/threads
+
+    split_array = np.array_split(data, parts)
+    pool = Pool(threads)
+    ast_string = []
+    ast_final = ""
+    for i in range(parts):
+        temp = "(function, {}, split_array[{}].reshape((split_array[{}].shape[0], 1)))".format(axis, i, i)
+        ast_string.append(temp)
+        ast_final += "eval(ast_string[{}]),".format(i)
+
+    result = pool.starmap(np.apply_along_axis, [eval(ast_string[0]),
+                                                eval(ast_string[1]),
+                                                eval(ast_string[2]),
+                                                eval(ast_string[3]) ])
+    result = np.concatenate(([*result]), axis=0)
+    return result
