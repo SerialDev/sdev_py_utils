8502f364a945b38be02c4ec7698254f3b85f8fd2
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-10-15 13:16:15 +0300

    [+] Load keras model + weights

--- sdev_scipy_utils/sdev_scipy_utils/keras_utils.py
+++ sdev_scipy_utils/sdev_scipy_utils/keras_utils.py
@@ -5,15 +5,30 @@ from keras import backend as K
 def root_mean_squared_error(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
 
+
 def coeff_determination(y_true, y_pred):
     from keras import backend as K
-    SS_res =  K.sum(K.square( y_true-y_pred ))
-    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
-    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+    SS_res = K.sum(K.square(y_true-y_pred))
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
+    return (1 - SS_res/(SS_tot + K.epsilon()))
 
 
 def tensorboard_callback(log_dir='./Graph'):
     return callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
 
+
 def early_stopping(monitor='loss'):
     return callbacks.EarlyStopping(monitor=monitor, patience=10, verbose=1, mode='auto')
+
+
+def model_with_weights(model, weights, skip_mismatch):
+    """ Load weights for model.
+
+    Args
+    model : The model to load weights for.
+    weights : The weights to load.
+    skip_mismatch : If True, skips layers whose shape of weights doesn't match with the model.
+    """
+    if weights is not None:
+        model.load_weights(weights, by_name=True, skip_mismatch=skip_mismatch)
+    return model
