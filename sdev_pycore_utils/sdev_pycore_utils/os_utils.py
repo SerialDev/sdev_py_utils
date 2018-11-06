1a081cd2e152acfe06e8a8a5bda1212bc9e6a25d
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-06 14:23:38 +0200

    [+] fn  convert_size

--- sdev_pycore_utils/sdev_pycore_utils/os_utils.py
+++ sdev_pycore_utils/sdev_pycore_utils/os_utils.py
@@ -2,13 +2,14 @@
 
 import fnmatch
 import glob
+import gzip
 import json
+import math
 import ntpath
 import os
 import re
 import shutil
 import subprocess
-import gzip
 from datetime import date, timedelta
 
 import dill as pickle  # Required to pickle lambda functions
@@ -960,3 +961,13 @@ def local_caching(data, name, force=False):
         with open(filepath, 'wb') as f:
             pickle.dump(data, f)
         return data
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
