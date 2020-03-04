c21af5409d5063000ce0d8a78dbbdfdc05256098
Serialdev <carlos.mariscal.melgar@gmail.com>
2020-03-04 07:22:47 -0800

    [+] Google cloud basic needed functionality

---
+++ sdev_lib_utils/sdev_lib_utils/gcloud_utils.py
@@ -0,0 +1,215 @@
+from google.cloud import storage
+import pprint
+
+
+def bucket_metadata(bucket_name):
+    """Prints out a bucket's metadata."""
+    # bucket_name = 'your-bucket-name'
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    print("ID: {}".format(bucket.id))
+    print("Name: {}".format(bucket.name))
+    print("Storage Class: {}".format(bucket.storage_class))
+    print("Location: {}".format(bucket.location))
+    print("Location Type: {}".format(bucket.location_type))
+    print("Cors: {}".format(bucket.cors))
+    print(
+        "Default Event Based Hold: {}".format(bucket.default_event_based_hold)
+    )
+    print("Default KMS Key Name: {}".format(bucket.default_kms_key_name))
+    print("Metageneration: {}".format(bucket.metageneration))
+    print(
+        "Retention Effective Time: {}".format(
+            bucket.retention_policy_effective_time
+        )
+    )
+    print("Retention Period: {}".format(bucket.retention_period))
+    print("Retention Policy Locked: {}".format(bucket.retention_policy_locked))
+    print("Requester Pays: {}".format(bucket.requester_pays))
+    print("Self Link: {}".format(bucket.self_link))
+    print("Time Created: {}".format(bucket.time_created))
+    print("Versioning Enabled: {}".format(bucket.versioning_enabled))
+    print("Labels:")
+    pprint.pprint(bucket.labels)
+
+def create_bucket(bucket_name):
+    """Creates a new bucket."""
+    # bucket_name = "your-new-bucket-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.create_bucket(bucket_name)
+
+    print("Bucket {} created".format(bucket.name))
+
+
+def list_buckets():
+    """Lists all buckets."""
+
+    storage_client = storage.Client()
+    buckets = storage_client.list_buckets()
+
+    for bucket in buckets:
+        print(bucket.name)
+
+
+
+def upload_blob_file(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+
+def list_blobs(bucket_name, show=False):
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name)
+
+    blobs_list = []
+    for blob in blobs:
+        if show:
+            print(blob.name)
+            blobs_list.append(blob)
+        else:
+            blobs_list.append(blob)
+    return blobs_list
+
+
+
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    """Lists all the blobs in the bucket that begin with the prefix.
+
+    This can be used to list all blobs in a "folder", e.g. "public/".
+
+    The delimiter argument can be used to restrict the results to only the
+    "files" in the given "folder". Without the delimiter, the entire tree under
+    the prefix is returned. For example, given these blobs:
+
+        a/1.txt
+        a/b/2.txt
+
+    If you just specify prefix = 'a', you'll get back:
+
+        a/1.txt
+        a/b/2.txt
+
+    However, if you specify prefix='a' and delimiter='/', you'll get back:
+
+        a/1.txt
+
+    Additionally, the same request will return blobs.prefixes populated with:
+
+        a/b/
+    """
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(
+        bucket_name, prefix=prefix, delimiter=delimiter
+    )
+
+    print("Blobs:")
+    for blob in blobs:
+        print(blob.name)
+
+    if delimiter:
+        print("Prefixes:")
+        for prefix in blobs.prefixes:
+            print(prefix)
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_blob_name, destination_file_name
+        )
+    )
+
+def rename_blob(bucket_name, blob_name, new_name):
+    """Renames a blob."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+    # new_name = "new-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    new_blob = bucket.rename_blob(blob, new_name)
+
+    print("Blob {} has been renamed to {}".format(blob.name, new_blob.name))
+
+
+
+def copy_blob(
+    bucket_name, blob_name, destination_bucket_name, destination_blob_name
+):
+    """Copies a blob from one bucket to another with a new name."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+    # destination_bucket_name = "destination-bucket-name"
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client()
+
+    source_bucket = storage_client.bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name
+    )
+
+    print(
+        "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+
+
+def delete_blob(bucket_name, blob_name):
+    """Deletes a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # blob_name = "your-object-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
+
+    print("Blob {} deleted.".format(blob_name))
