6a20262eb5ef6f9288b7cd9017abff9a67c8da1f
Andres <amariscal@cloudflare.com>
2023-04-26 19:24:06 +0100

    [+] Functional utils docstrings and usage

--- sdev_pycore_utils/sdev_pycore_utils/functional_utils.py
+++ sdev_pycore_utils/sdev_pycore_utils/functional_utils.py
@@ -75,29 +75,27 @@ def maybe(func, *args, **kwargs):
 
 def match(value, patterns):
     """
-    Works like the 'match' keyword in Rust. Evaluates each pattern function in the 'patterns' list with the 'value' as an argument.
-    If a pattern function returns True, the corresponding action function is called with the 'value' as an argument and the result is returned.
-    If no matching pattern is found, a message is printed indicating that no matching pattern was found and 0 is returned.
-    If an exception occurs while evaluating a pattern-action pair, the error results are printed and 0 is returned.
-    
-    Example:
-    def is_even(x):
-        return x % 2 == 0
-    def double(x):
-        return x * 2
-    def triple(x):
-        return x * 3
-    value = 5
-    patterns = [(is_even, double), (lambda x: True, triple)]
-    result = match(value, patterns)
-    print(result)  # Output: 15
-    
-    :param value: The value to be matched
-    :type value: Any
-    :param patterns: A list of pattern-action pairs, where each pair consists of a pattern function and an action function
-    :type patterns: List[Tuple[Callable[[Any], bool], Callable[[Any], Any]]]
-    :return: The result of calling the action function corresponding to the first matching pattern function, or 0 if no matching pattern is found or an exception occurs
-    :rtype: Any
+    * ---------------{Function}---------------
+    * Works like the 'match' keyword in Rust. Evaluates each pattern function in the 'patterns' list with the 'value' as an argument.
+    * If a pattern function returns True, the corresponding action function is called with the 'value' as an argument and the result is returned.
+    * If no matching pattern is found, a message is printed indicating that no matching pattern was found and 0 is returned.
+    * If an exception occurs while evaluating a pattern-action pair, the error results are printed and 0 is returned.
+    * ----------------{Returns}---------------
+    * -> result    ::Any        |The result of calling the action function corresponding to the first matching pattern function, or 0 if no matching pattern is found or an exception occurs
+    * ----------------{Params}----------------
+    * : value      ::Any        |The value to be matched
+    * : patterns   ::List[Tuple]|A list of pattern-action pairs, where each pair consists of a pattern function and an action function
+    * ----------------{Usage}-----------------
+    * def is_even(x):
+    *     return x % 2 == 0
+    * def double(x):
+    *     return x * 2
+    * def triple(x):
+    *     return x * 3
+    * value = 5
+    * patterns = [(is_even, double), (lambda x: True, triple)]
+    * result = match(value, patterns)
+    * print(result)  # Output: 15
     """
     for pattern, action in patterns:
         try:
@@ -115,18 +113,21 @@ def match(value, patterns):
 
 # ---{Compose Higher order functions}---#
 
-
 def compose(*funcs):
-    """Return a new function s.t.
-       compose(f,g,...)(x) == f(g(...(x)))
-
-    >>> times2 = lambda x: x*2
-    >>> minus3 = lambda x: x-3
-    >>> mod6 = lambda x: x%6
-    >>> f = compose(mod6, times2, minus3)
-    >>> all(f(i)==((i-3)*2)%6 for i in range(1000000))
-    True
-
+    """
+    * ---------------{Function}---------------
+    * Return a new function s.t. compose(f,g,...)(x) == f(g(...(x)))
+    * ----------------{Returns}---------------
+    * -> result    ::Callable   |The composed function
+    * ----------------{Params}----------------
+    * : *funcs     ::*Callable  |Functions to be composed
+    * ----------------{Usage}-----------------
+    * >>> times2 = lambda x: x*2
+    * >>> minus3 = lambda x: x-3
+    * >>> mod6 = lambda x: x%6
+    * >>> f = compose(mod6, times2, minus3)
+    * >>> all(f(i)==((i-3)*2)%6 for i in range(1000000))
+    * True
     """
 
     def inner(data, funcs=funcs):
