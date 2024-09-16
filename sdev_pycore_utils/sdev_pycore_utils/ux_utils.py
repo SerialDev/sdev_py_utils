42c62adea9469f2275b2d6b36d615e110c5892ff
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-09-16 12:22:06 +0100

    [+] Added ux_utils.py module with pprint_dict function to pretty print dictionaries with alternating colors.

---
+++ sdev_pycore_utils/sdev_pycore_utils/ux_utils.py
@@ -0,0 +1,35 @@
+"""Python core ux  utilitites"""
+
+
+def pprint_dict(
+    d,
+    key_color1="\033[92m",
+    key_color2="\033[93m",
+    value_color1="\033[95m",
+    value_color2="\033[96m",
+    reset="\033[0m",
+):
+    """
+    Pretty print a dictionary with alternating colors.
+
+    Args:
+        d (dict): The dictionary to be printed.
+        key_color1 (str, optional): The first key color. Defaults to bright red ('\033[91m').
+        key_color2 (str, optional): The second key color. Defaults to bright yellow ('\033[93m').
+        value_color1 (str, optional): The first value color. Defaults to bright magenta ('\033[95m').
+        value_color2 (str, optional): The second value color. Defaults to bright cyan ('\033[96m').
+        reset (str, optional): The reset code. Defaults to '\033[0m'.
+
+    Usage:
+        >>> d = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+        >>> pretty_print_dict(d)
+        a : 1
+        b : 2
+        c : 3
+        d : 4
+        e : 5
+    """
+    for i, (key, value) in enumerate(d.items()):
+        key_color = key_color1 if i % 2 == 0 else key_color2
+        value_color = value_color1 if i % 2 == 0 else value_color2
+        print(f"{key_color}{key}{reset} : {value_color}{value}{reset}")
