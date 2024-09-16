b6cefc38bada2b24c709545550aa93a53681dd5d
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-09-16 14:14:33 +0100

    [+] Added pprint_model_layers function to ux_utils.py for pretty-printing model layers.

--- sdev_pycore_utils/sdev_pycore_utils/ux_utils.py
+++ sdev_pycore_utils/sdev_pycore_utils/ux_utils.py
@@ -33,3 +33,51 @@ def pprint_dict(
         key_color = key_color1 if i % 2 == 0 else key_color2
         value_color = value_color1 if i % 2 == 0 else value_color2
         print(f"{key_color}{key}{reset} : {value_color}{value}{reset}")
+
+
+def pprint_model_layers(
+    model,
+    key_color1="\033[92m",
+    key_color2="\033[93m",
+    value_color1="\033[95m",
+    value_color2="\033[96m",
+    reset="\033[0m",
+    level=0,
+):
+    import numpy as np
+    import torch
+    import mlx.core as mx
+
+    # Handle numpy arrays, PyTorch tensors, and MLX arrays
+    if isinstance(model, (np.ndarray, torch.Tensor, mx.array)):
+        model = model.cpu().numpy() if isinstance(model, torch.Tensor) else model
+        array_str = (
+            f"[{', '.join(map(str, model[:2]))} ...]" if model.size > 2 else str(model)
+        )
+        print(f"{' ' * level}{value_color1}{array_str}{reset}")
+    elif isinstance(model, dict):
+        for name, module in model.items():
+            print(f"{key_color1}{' ' * level}{name}{reset} :")
+            pprint_model_layers(
+                module,
+                key_color2,
+                key_color1,
+                value_color2,
+                value_color1,
+                reset,
+                level + 2,
+            )
+    elif isinstance(model, list):
+        for idx, item in enumerate(model):
+            print(f"{key_color1}{' ' * level}[{idx}]{reset} :")
+            pprint_model_layers(
+                item,
+                key_color2,
+                key_color1,
+                value_color2,
+                value_color1,
+                reset,
+                level + 2,
+            )
+    else:
+        print(f"{' ' * level}{value_color1}{model}{reset}")
