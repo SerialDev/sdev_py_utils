b78d29eeb4bcf879be77ddaa5220355a5a6ffe7e
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-10-12 11:01:16 +0100

    [$] Refactored embed_utils.py to simplify Node2Vec model initialization and improved docstrings for better code readability.

--- sdev_scipy_utils/sdev_scipy_utils/embed_utils.py
+++ sdev_scipy_utils/sdev_scipy_utils/embed_utils.py
@@ -1,13 +1,9 @@
-
-
-
 def embed_to_nod2vec(
     subgraph, dimensions=64, walk_length=30, num_walks=200, workers=4, dim=0
 ):
     import torch
     from node2vec import Node2Vec
 
-    # Initialize Node2Vec model
     node2vec = Node2Vec(
         subgraph,
         dimensions=dimensions,
@@ -17,13 +13,10 @@ def embed_to_nod2vec(
     )
     model = node2vec.fit(window=10, min_count=1, batch_words=4)
 
-    # Retrieve all node embeddings
     all_embeddings = {node: model.wv[node] for node in model.wv.index_to_key}
 
-    # Convert each numpy array to a PyTorch tensor
     tensor_list = [torch.from_numpy(arr) for arr in all_embeddings.values()]
 
-    # Stack the tensors
     embeddings_tensor = torch.stack(tensor_list)
 
     # Calculate graph embedding
@@ -32,15 +25,12 @@ def embed_to_nod2vec(
     return graph_embedding
 
 
-
-
 def embed_nod2vec_dot_product(
     subgraph, dimensions=64, walk_length=30, num_walks=200, workers=4
 ):
     import torch
     from node2vec import Node2Vec
 
-    # Initialize Node2Vec model
     node2vec = Node2Vec(
         subgraph,
         dimensions=dimensions,
@@ -50,13 +40,10 @@ def embed_nod2vec_dot_product(
     )
     model = node2vec.fit(window=10, min_count=1, batch_words=4)
 
-    # Retrieve all node embeddings
     all_embeddings = {node: model.wv[node] for node in model.wv.index_to_key}
 
-    # Convert each numpy array to a PyTorch tensor
     tensor_list = [torch.from_numpy(arr) for arr in all_embeddings.values()]
 
-    # Stack the tensors
     embeddings_tensor = torch.stack(tensor_list)
 
     # Calculate the dot product between each pair of embeddings
@@ -70,17 +57,16 @@ def embed_nod2vec_dot_product(
     return dot_products, upper_tri_vector
 
 
-
 def pd_get_embeddings(df, column, batch_size=32, as_dataframe=False):
-    '''
+    """
     * ---------------Function---------------
     * Computes sentence embeddings using a pre-trained BERT model.
-    * 
-    * The function takes a pandas DataFrame and a column name as input, 
-    * processes the data in chunks, and computes the sentence embeddings 
-    * using the BERT model. The embeddings can be returned as a numpy array 
+    *
+    * The function takes a pandas DataFrame and a column name as input,
+    * processes the data in chunks, and computes the sentence embeddings
+    * using the BERT model. The embeddings can be returned as a numpy array
     * or a pandas DataFrame.
-    * 
+    *
     * ----------------Params----------------
     * df : pandas.DataFrame
     *     Input DataFrame.
@@ -90,21 +76,21 @@ def pd_get_embeddings(df, column, batch_size=32, as_dataframe=False):
     *     Batch size for processing data.
     * as_dataframe : bool, optional (default=False)
     *     Whether to return the embeddings as a pandas DataFrame or a numpy array.
-    * 
+    *
     * ----------------Returns---------------
     * -> pd.DataFrame or np.ndarray
-    *     Sentence embeddings in a pandas DataFrame or a numpy array, 
+    *     Sentence embeddings in a pandas DataFrame or a numpy array,
     *     depending on the `as_dataframe` parameter.
-    * 
+    *
     * ----------------Usage-----------------
-    * 
+    *
     * ```
     * import pandas as pd
     * from transformers import AutoTokenizer, AutoModel
-    * 
+    *
     * tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
     * model = AutoModel.from_pretrained('bert-base-uncased')
-    * 
+    *
     * df = pd.DataFrame({'lead_contact_title': ['example sentence 1', 'example sentence 2']})
     * embeddings_df = pd_get_embeddings(df, 'lead_contact_title', 512, as_dataframe=True)
     * ```
@@ -117,42 +103,41 @@ def pd_get_embeddings(df, column, batch_size=32, as_dataframe=False):
     # Concatenate the original DataFrame with the embeddings DataFrame
     #temp_df = pd.concat([temp_df, embeddings_df], axis=1)
     #embedding_columns = embeddings_df.columns.tolist()
-    * 
+    *
     * ----------------Notes-----------------
     * This function assumes that the input DataFrame column contains strings.
-    * The BERT model and tokenizer are not included in the function signature; 
+    * The BERT model and tokenizer are not included in the function signature;
     * they need to be defined before calling this function.
-    '''
+    """
 
     df[column] = df[column].astype(str)
 
     embeddings_list = []
 
-    # Process the data in chunks
     for i in tqdm(range(0, len(df), batch_size)):
-        batch = df[i:i+batch_size]
-        inputs = tokenizer(list(batch[column]), padding=True, truncation=True, max_length=128, return_tensors='pt')
+        batch = df[i : i + batch_size]
+        inputs = tokenizer(
+            list(batch[column]),
+            padding=True,
+            truncation=True,
+            max_length=128,
+            return_tensors="pt",
+        )
 
-        # Get the embeddings
         with torch.no_grad():
             embeddings = model(**inputs).last_hidden_state
 
         # Compute mean of all token embeddings for each sentence to create sentence embeddings
         sentence_embeddings = embeddings.mean(dim=1)
 
-        # Convert tensor to numpy array and append to list
         numpy_embeddings = sentence_embeddings.numpy()
         embeddings_list.append(numpy_embeddings)
-    
-    # Concatenate all the chunks
+
     all_embeddings = np.concatenate(embeddings_list, axis=0)
-    
     if as_dataframe:
         num_cols = all_embeddings.shape[1]
-        cols = [f'{column}_embed_{i}' for i in range(num_cols)]
+        cols = [f"{column}_embed_{i}" for i in range(num_cols)]
         df_embeddings = pd.DataFrame(all_embeddings, columns=cols)
         return df_embeddings
 
     return all_embeddings
-
-
